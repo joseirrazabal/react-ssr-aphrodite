083f77f738224b2cdeed1af4f69c8a72628a002d
Jose Luis Irrazabal <joseirrazabal@gmail.com>
2018-06-15 17:32:10 -0300

    maurio sin lib

--- src/containers/Prueba.js
+++ src/containers/Prueba.js
@@ -4,9 +4,9 @@ import PropTypes from "prop-types";
 import ErrorBoundary from "../errorBoundary";
 import cssGlobal from "../../css/styles.scss";
 
-import SearchHotels from "Maurio/lib/Organisms/Search/SearchHotels";
-import SearchPaquetes from "Maurio/lib/Organisms/Search/SearchPaquetes";
-// import SearchVuelos from "Maurio/lib/Organisms/Search/SearchVuelos";
+import SearchHotels from "Maurio/Organisms/Search/SearchHotels";
+import SearchPaquetes from "Maurio/Organisms/Search/SearchPaquetes";
+// import SearchVuelos from "Maurio/Organisms/Search/SearchVuelos";
 
 class App extends Component {
   render() {
